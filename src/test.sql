c33f3feb0496db91b6a5f2e9f38d3c6e6856a9a4
Jazzy-girl <rsw5713@g.rit.edu>
2025-04-19 17:45:37 -0400

    Made a partial .sql file!

---
+++ src/test.sql
@@ -0,0 +1,24 @@
+CREATE TABLE riders (
+    id  SERIAL PRIMARY KEY NOT NULL,
+    rider_name VARCHAR(30) NOT NULL DEFAULT 'John Doe',
+    instructions VARCHAR(50) NOT NULL DEFAULT 'No special instructions',
+    rating VARCHAR(3) NOT NULL DEFAULT '0.0',
+    credit_card VARCHAR(16) NOT NULL 
+)
+
+CREATE TABLE drivers (
+    id  SERIAL PRIMARY KEY NOT NULL,
+    driver_name VARCHAR(30) NOT NULL DEFAULT 'Jane Doe',
+    instructions VARCHAR(50),
+    rating VARCHAR(3) NOT NULL DEFAULT '0.0',
+    make_model VARCHAR(16) NOT NULL,
+    duration VARCHAR(5) NOT NULL,
+    license VARCHAR(10) NOT NULL DEFAULT 'abc123'
+)
+
+INSERT INTO drivers(driver_name, instructions, rating)
+    VALUES ('Tom Magliozzi', 'Don''t drive like my brother', '3.2'),
+            ('Ray Magliozzi', 'Don''t drive like my brother', '3.4')
+
+INSERT INTO riders(rider_name, rating)
+    VALUE ('Mike Easter', '4.3')
