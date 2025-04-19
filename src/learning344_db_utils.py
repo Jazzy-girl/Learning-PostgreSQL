9e84d8228d40b448a717544eba91182a88e47fac
Jazzy-girl <rsw5713@g.rit.edu>
2025-04-19 18:02:21 -0400

    added read_sql_file method to utils

--- src/learning344_db_utils.py
+++ src/learning344_db_utils.py
@@ -2,10 +2,28 @@ import psycopg2
 import yaml
 import os
 
-"""
-Connects you to Postgres via config. Closing this connection is up to you.
-"""
+
+
+
+def read_sql_file(file_path):
+    """
+    Opens and reads an sql file. Returns the file as a string to be used for commands.
+    """
+    try:
+        with open(file_path, "r") as file:
+            sql_content = file.read()
+        return sql_content
+    except FileNotFoundError:
+        print(f"Error: SQL file not found at {file_path}")
+    except Exception as e:
+        print(f"Error reading SQL file: {e}")
+        exit()
+
+
 def connect():
+    """
+    Connects you to Postgres via config. Closing this connection is up to you.
+    """
     config = {}
     yml_path = os.path.join(os.path.dirname(__file__), '../config/db.yml')
     with open(yml_path, 'r') as file:
@@ -16,11 +34,12 @@ def connect():
                             host=config['host'],
                             port=config['port'])
 
-"""
+
+def exec_sql_file(path):
+    """
 Opens up an SQL file and blindly executes everything inside. Useful for test data and your schema.
 Having your code in an SQL file also gives syntax highlighting!
 """
-def exec_sql_file(path):
     full_path = os.path.join(os.path.dirname(__file__), f'../../{path}')
     conn = connect()
     cur = conn.cursor()
@@ -29,11 +48,12 @@ def exec_sql_file(path):
     conn.commit()
     conn.close()
 
-"""
+
+def exec_get_one(sql, args={}):
+    """
 Runs a query and assumes that you only want the top result and return that.
 Does NOT commit any changes (don't use it for updates).
 """
-def exec_get_one(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     cur.execute(sql, args)
@@ -41,11 +61,12 @@ def exec_get_one(sql, args={}):
     conn.close()
     return one
 
-"""
+
+def exec_get_all(sql, args={}):
+    """
 Runs a query and returns all results, usually as a list of tuples.
 Does NOT commit any changes (don't use it for updates).
 """
-def exec_get_all(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     cur.execute(sql, args)
@@ -54,10 +75,11 @@ def exec_get_all(sql, args={}):
     conn.close()
     return list_of_tuples
 
-"""
+
+def exec_commit(sql, args={}):
+    """
 Runs SQL and does a commit operation (use this for updating/changing the database).
 """
-def exec_commit(sql, args={}):
     conn = connect()
     cur = conn.cursor()
     result = cur.execute(sql, args)
